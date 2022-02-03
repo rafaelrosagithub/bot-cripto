e8172ffbaec607bf4e4cea2868b041ab756b929c
Rafael <rafael_unis@hotmail.com>
2022-02-02 22:37:49 -0300

    Connection to telegran api

--- index.js
+++ index.js
@@ -2,17 +2,32 @@
 // 36291 
 // 35764
 
+
+// https://api.telegram.org/bot<here you put your token>/getUpdates
+
+const TOKEN = "<here you put your token>";
+const CHAT_ID = 1036917056;
+let lastmessage = "";
+
 async function process() {
+    const { Telegraf } = require("telegraf");
+    const bot = new Telegraf(TOKEN);
+
     const axios = require("axios");
+
     const response = await axios.get("https://api.binance.com/api/v3/klines?symbol=BTCBUSD&interval=1m");
     const candle = response.data[499];
     const price = parseFloat(candle[4]);
-    if (price >= 36291) {
-        console.log("Vender !");
-    } else if (price <= 35764) {
-        console.log("Comprar!")
+    if (price >= 36291 && lastmessage !== "Time to sell!") {
+        lastmessage = "Time to sell!";
+        console.log(lastmessage);  
+        bot.telegram.sendMessage(CHAT_ID, lastmessage);
+    } else if (price <= 35764 && lastmessage !== "Time to buy!") {
+        lastmessage = "Time to buy!";
+        console.log(lastmessage)
+        bot.telegram.sendMessage(CHAT_ID, lastmessage);
     } else {
-        console.log("Aguardar!");
+        console.log("Wait...");
     }
     console.log(candle[4]);
 }
